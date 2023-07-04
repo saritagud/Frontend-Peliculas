71da39000aaf133641ccdeb9650370cff532d9eb
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-07-03 23:18:01 -0400

    Formateado

--- Frontend/src/components/Movies/CardMovie.jsx
+++ Frontend/src/components/Movies/CardMovie.jsx
@@ -1,15 +1,16 @@
 import { formatearFecha } from '../../logic/funciones'
 import { Link } from 'react-router-dom'
+import ButtonDetails from './ButtonDetails'
 
 function CardMovie({ data }) {
    const { _id, imagen, titulo, fechaPublicacion } = data
    return (
       <>
          <section className=' m-6 text-white w-[60%] '>
-            <div className="bg-Card bg-cover flex flex-col justify-center items-center  rounded-xl h-[60vh] w-[80%]">
-                <div className="flex flex-col justify-end items-center p-5 bg-black/75 w-full h-full rounded-xl opacity-0 hover:opacity-100 transition-all duration-500 cursor-pointer">
-                    <ButtonDetails />
-                </div>
+            <div className='bg-Card bg-cover flex flex-col justify-center items-center  rounded-xl h-[60vh] w-[80%]'>
+               <div className='flex flex-col justify-end items-center p-5 bg-black/75 w-full h-full rounded-xl opacity-0 hover:opacity-100 transition-all duration-500 cursor-pointer'>
+                  <ButtonDetails />
+               </div>
             </div>
             <h1 className='font-Coda mt-3 text-left w-full tracking-wide'>
                {titulo}
