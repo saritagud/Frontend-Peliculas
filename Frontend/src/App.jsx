c78cdf3141a7072be90db3f45ad154789fa653a7
saritagud <gudinop864@gmail.com>
2023-07-05 16:57:01 -0400

    agregado de nuevo las configuraciones del app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,11 +4,12 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import DetailsMovie from "./components/Movies/DetailsMovie";
 import Dashboard from "./components/Admin/Dashboard";
-
+import { useSelector } from "react-redux";
+import ProtectedRouter from "./components/Admin/ProtectedRouter";
 import "./index.css";
 
 function App() {
-  
+  const user = useSelector((state) => state.user);
   return (
     <div className="min-h-screen bg-fondo">
       <Routes>
@@ -19,9 +20,9 @@ function App() {
         <Route
           path="/administrador"
           element={
-            
+            <ProtectedRouter isAllowed={user.user.rol === "admin"}>
               <Dashboard />
-            
+            </ProtectedRouter>
           }
         />
       </Routes>
