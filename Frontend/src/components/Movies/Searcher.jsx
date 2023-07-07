bbe60bd7dcc0e499a3a4567e5153a2ed5a88d138
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-07-07 07:27:47 -0400

    Buscador actualizado

--- Frontend/src/components/Movies/Searcher.jsx
+++ Frontend/src/components/Movies/Searcher.jsx
@@ -1,15 +1,30 @@
 import { FaSearch } from "react-icons/fa";
+import { useDispatch } from "react-redux";
+import { filterMovies } from "../../features/movies/moviesSlice";
 
 function Searcher() {
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const search = {
+      palabraClave: e.target.search.value
+    }
+    dispatch(filterMovies(search))
+  }
   return (
     <>
       <section className="flex justify-end items-center w-full p-8">
-        <input
-          type="text"
-          placeholder="Buscar peliculas"
-          className="p-3 rounded-xl bg-verde text-white placeholder-white mr-5 font-Marcellus text-lg w-[60%] sm:w-[40%] md:text-xl xl:text-2xl xl:w-[30%] 2xl:text-3xl 2xl:p-5 2xl:mr-20 "
-        ></input>
-        <FaSearch className="text-white -ml-14 text-lg 2xl:text-3xl 2xl:-ml-32" />
+        <form className="flex justify-end items-center w-full p-8" onSubmit={handleSubmit}>
+          <input
+            name="search"
+            type="text"
+            placeholder="Buscar peliculas"
+            className="p-3 rounded-xl bg-verde text-white placeholder-white mr-5 font-Marcellus text-lg w-[60%] sm:w-[40%] md:text-xl xl:text-2xl xl:w-[30%] 2xl:text-3xl 2xl:p-5 2xl:mr-20 "
+          />
+          <button>
+            <FaSearch className="text-white -ml-14 text-lg 2xl:text-3xl 2xl:-ml-32" />
+          </button>
+        </form>
       </section>
     </>
   );
